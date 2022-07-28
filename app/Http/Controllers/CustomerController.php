2540fd4b4aa8d9ac008c7092e260a1d16d9454b5
Thomas Jimenez C <tom.jimenez1661@gmail.com>
2022-07-28 16:45:56 -0600

    Add Customer and Category Controller resources

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $customers = Customer::get();
+
+        return view('customers.index')
+                ->with('customers', $customers)
+                ->with('title', 'Lista de Clientes');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('customers.create')
+                ->with('title', 'Registro de un nuevo cliente');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $customer = new Customer;
+
+        $customer->name = $request->input('name');
+        $customer->address = $request->input('address');
+        $customer->phone_number = $request->input('phone_number');
+
+        $customer->save();
+
+        return redirect()->route('customers.index');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Customer $customer)
+    {
+        return view('customers.edit')
+                ->with('customer', $customer)
+                ->with('title', 'Actualizar datos del Cliente');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Customer $customer)
+    {
+        $customer->name = $request->input('name');
+        $customer->address = $request->input('address');
+        $customer->phone_number = $request->input('phone_number');
+
+        $customer->save();
+
+        return redirect()->route('customers.index');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $customer = Customer::findOrFail($id);
+
+        $customer->delete();
+        
+        return redirect()->route('customers.index');
+
+    }
+}
